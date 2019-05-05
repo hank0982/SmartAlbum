b595e04aea395b5b910c929ecfcdd8cd659d2855
hank0982 <hank0982@gmail.com>
2019-05-05 23:07:05 +0800

    Fix Album Details

--- app/src/main/java/cse/cuhk/smartalbum/SearchFragment.java
+++ app/src/main/java/cse/cuhk/smartalbum/SearchFragment.java
@@ -42,15 +42,15 @@ public class SearchFragment extends Fragment {
         //title.setX(getResources().getDimensionPixelSize(R.dimen.left_offset));
         //title.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "open-sans-extrabold.ttf"));
         final FloatingSearchView mSearchView = view.findViewById(R.id.floating_search_view);
-        mSearchView.setBackgroundColor(Color.parseColor("#787878"));
-        mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
-        mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
-        mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
-        mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
-        mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
-        mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
-        mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
-        mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setBackgroundColor(Color.parseColor("#787878"));
+//        mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
+//        mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
+//        mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
         mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
             public void onSearchTextChanged(String oldQuery, final String newQuery) {
